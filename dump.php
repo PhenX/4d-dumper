0611396e98ada8e2ed59909b067e420f0db95056
PhenX <fabien.menager@gmail.com>
2016-12-07 14:51:45 +0100

    Added possibility to export the schema as SQL file Remove the diacritics in column names Add slashes in fputcsv All tables where not exported (only upper case ones)

--- dump.php
+++ dump.php
@@ -20,6 +20,7 @@ $options = array(
   "username" => "administrateur",
   "password" => null,
   "output"   => ".",
+  "schemaout" => null,
   "limit"    => 1000000,
   "tables"   => null,
 );
@@ -35,6 +36,7 @@ for ($i = 1; $i < $argc; $i++) {
     case "--username":
     case "--password":
     case "--output":
+    case "--schemaout":
     case "--limit":
     case "--tables":
       $options[substr($argv[$i], 2)] = $argv[++$i];
@@ -57,11 +59,6 @@ if (empty($options["tables"])) {
   $list_tables = array();
   while ($row = $stmt->fetchRow()) {
     $_table_name = $row["TABLE_NAME"];
-
-    if (in_array($_table_name, array("PARAMETRES"))) {
-      continue;
-    }
-
     $list_tables[] = $_table_name;
   }
 }
@@ -76,24 +73,59 @@ if (!is_dir($output)) {
   mkdir($output);
 }
 
+$schemaout = $options["schemaout"];
+if (!is_dir(dirname($schemaout))) {
+  mkdir(dirname($schemaout));
+}
+
+$schema = null;
+if ($schemaout) {
+  $schema = fopen($schemaout, "w");
+}
+
+$allographs = array(
+  "withdiacritics"    => "àáâãäåòóôõöøèéêëçìíîïùúûüÿñ",
+  "withoutdiacritics" => "aaaaaaooooooeeeeciiiiuuuuyn",
+);
+
 foreach ($list_tables as $_table) {
   echo "## $_table ##\n";
 
   // List columns
   $_table_name = strtoupper($_table);
-  $stmt_cols = $client->execStatement("SELECT * FROM _USER_COLUMNS WHERE TABLE_NAME = '$_table_name';");
+  $stmt_cols = $client->execStatement("SELECT * FROM _USER_COLUMNS WHERE UPPER(TABLE_NAME) = '$_table_name';");
   $cols = $stmt_cols->fetchAll();
   $stmt_cols->close();
 
   // List indexes
-  $stmt_indexes = $client->execStatement("SELECT * FROM _USER_INDEXES WHERE TABLE_NAME = '$_table_name';");
+  $stmt_indexes = $client->execStatement("SELECT * FROM _USER_INDEXES WHERE UPPER(TABLE_NAME) = '$_table_name';");
   $indexes = $stmt_indexes->fetchAll();
   $stmt_indexes->close();
+
+  $stmt_index_columns = $client->execStatement("SELECT * FROM _USER_IND_COLUMNS WHERE UPPER(TABLE_NAME) = '$_table_name';");
+  $index_columns = $stmt_index_columns->fetchAll();
+  $stmt_index_columns->close();
+
+  $index_struct = array();
+  foreach ($indexes as $_index) {
+    $_id = $_index["INDEX_ID"];
+    $index_struct[] = array(
+      "columns" => array_filter(
+        $index_columns,
+        function ($v) use ($_id) {
+          return $v["INDEX_ID"] === $_id;
+        }
+      ),
+      "type"   => $_index["INDEX_TYPE"],
+      "name"   => $_index["INDEX_NAME"],
+      "unique" => $_index["UNIQUENESS"],
+    );
+  }
 
   $query = "CREATE TABLE `$_table`";
   $query_columns = array();
   foreach ($cols as $_col) {
-    $_name = $_col["COLUMN_NAME"];
+    $_name = str_replace($allographs["withdiacritics"], $allographs["withoutdiacritics"], $_col['COLUMN_NAME']);
     $_type = $_col["DATA_TYPE"];
     $_length = $_col["DATA_LENGTH"];
     $_nullable = $_col["NULLABLE"];
@@ -112,6 +144,7 @@ foreach ($list_tables as $_table) {
      * Alpha              10 *
      * Text               10 *
      * Picture            12
+     * CLOB               14
      * BLOB               18
      */
     switch ($_type) {
@@ -151,6 +184,7 @@ foreach ($list_tables as $_table) {
         }
         break;
       case 12:
+      case 14:
       case 18:
         $_sql_type = "BLOB";
         break;
@@ -162,18 +196,55 @@ foreach ($list_tables as $_table) {
     $query_columns[] = "`$_name` $_sql_type";
   }
 
-  $query .= "(\n  ".implode(",\n  ", $query_columns)."\n);";
+  // Indexes
+  foreach ($index_struct as $_struct) {
+    $_columns = array();
+    foreach ($_struct["columns"] as $_col) {
+      $_name = str_replace($allographs["withdiacritics"], $allographs["withoutdiacritics"], $_col['COLUMN_NAME']);
+      $_columns[] = "`$_name`";
+    }
 
-  file_put_contents("$output/$_table.sql", $query);
+    $_type = "INDEX";
+    if ($_struct["unique"]) {
+      $_type = "UNIQUE";
+    }
+
+    $query_columns[] = "$_type (".implode(', ', $_columns).")";
+  }
+
+  if (count($query_columns)) {
+    $query .= " (\n  ".implode(",\n  ", $query_columns)."\n) /*! ENGINE=MyISAM */;";
+
+    if ($schema) {
+      fwrite($schema, "-- $_table --\n$query\n\n");
+    }
+    else {
+      file_put_contents("$output/$_table.sql", $query);
+    }
+  }
 
   $stmt = $client->execStatement("SELECT * FROM $_table LIMIT $limit;");
 
   $csv = fopen("$output/$_table.csv", "w");
 
+  $_columns = array_map(
+    function ($v) use ($allographs) {
+      return str_replace($allographs["withdiacritics"], $allographs["withoutdiacritics"], $v);
+    },
+    $stmt->getColumns()
+  );
+
   $s = fputcsv($csv, $stmt->getColumns());
 
   $n = 0;
   while ($row = $stmt->fetchRow()) {
+    $row = array_map(
+      function ($v) {
+        return str_replace('\\', '\\\\', $v);
+      },
+      $row
+    );
+
     $s += fputcsv($csv, $row);
 
     if ($n % 500 === 0) {
@@ -182,6 +253,8 @@ foreach ($list_tables as $_table) {
 
     $n++;
   }
+
+  fclose($csv);
 
   echo sprintf(" -> %d rows exported \tFile size: %s\r", $n, number_format($s, 0, ".", " "));
 
